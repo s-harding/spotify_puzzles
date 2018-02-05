0a4d846376bbda19549117b59455b6c60645606d
Shane <hardingshane@gmail.com>
2018-02-05 20:45:28 +0000

    reformatting from jupyter notebook format

--- spotify_puzzle_1.py
+++ spotify_puzzle_1.py
@@ -1,33 +1,12 @@
 
 # coding: utf-8
 
-# In[32]:
+def reverse_bin(x):
+  bin(x)
+  str_x = str(bin(x))
+  reverse_x = str_x[::-1]
+  ans_bin = reverse_x[:len(reverse_x)-2]
+  return int(ans_bin,2)
 
-
-x = 47
-bin(x)
-
-
-# In[33]:
-
-
-str_x = str(bin(x))
-
-
-# In[34]:
-
-
-reverse_x = str_x[::-1]
-
-
-# In[35]:
-
-
-ans_bin = reverse_x[:len(reverse_x)-2]
-
-
-# In[37]:
-
-
-int(ans_bin,2)
-
+x = input("Please enter an integer:")
+print("The binary reverse of {} is {}".format(x, reverse_bin(int(x))))
