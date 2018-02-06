ef57b49c7428aeeb77bd9d07d69f2c42324a0a4b
Shane <hardingshane@gmail.com>
2018-02-06 18:51:31 +0000

    problem2 - fixed zipfs func

--- spotify_puzzle_2.py
+++ spotify_puzzle_2.py
@@ -1,14 +1,22 @@
 
+import copy
+
+
 #placeholder - file input to go here
-album1 = [[30,'one'],[30,'two'],[15,'three'],[25,'four']]
+album1 = [[30.,'one'],[30.,'two'],[15.,'three'],[25.,'four']]
 
 def zipfs_law(album, top_n):
-  q = album
+  q = copy.deepcopy(album)
+  ans = []
+  expt = []
   for i in range(len(album)):
-    q[i][0] = (album[i][0]/(i+1))
-  sorted(q, key = lambda l:l[1], reverse=True)
-  print(q[:top_n])
-  return q[:top_n]
+    val = album[0][0]/float((i+1.))
+    #print(val)
+    expt.append(val)
+    q[i][0] = album[i][0]/expt[i]
+  q = sorted(q, key=lambda x: x[0], reverse=True)
+
+  ans = [sublist[1:] for sublist in q[:top_n]]
+  return ans
   
-  
-zipfs_law(album1, 2)
+print(zipfs_law(album1, 2))
