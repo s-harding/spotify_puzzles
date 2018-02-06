8b33b6207a2273ec8ba25e1e70602b54e1996509
Shane <hardingshane@gmail.com>
2018-02-06 08:23:25 +0000

    problem2 - return sorted q

--- spotify_puzzle_2.py
+++ spotify_puzzle_2.py
@@ -1,12 +1,14 @@
 
+#placeholder - file input to go here
 album1 = [[30,'one'],[30,'two'],[15,'three'],[25,'four']]
 
-def zipfs_law(album):
-  q = []
+def zipfs_law(album, top_n):
+  q = album
   for i in range(len(album)):
-    q.append(album[i][0]/(i+1))
-  print(q)
-  return q
+    q[i][0] = (album[i][0]/(i+1))
+  sorted(q, key = lambda l:l[1], reverse=True)
+  print(q[:top_n])
+  return q[:top_n]
   
   
-zipfs_law(album1)
+zipfs_law(album1, 2)
